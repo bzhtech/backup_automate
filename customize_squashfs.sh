501d57fcc890a97824d70281a4c2737284b4a8a5
bzhtech <tangi@bzhtech.eu>
2022-09-25 15:07:04 +0200

    correction diverses

--- customize_squashfs.sh
+++ customize_squashfs.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-dest="tftp/clonezilla"
+dest="clonezilla"
 mkdir $dest
 tmp=$$
 wget https://freefr.dl.sourceforge.net/project/clonezilla/clonezilla_live_stable/3.0.1-8/clonezilla-live-3.0.1-8-amd64.iso
 sudo mkdir /mnt/iso
 sudo mount clonezilla-live-3.0.1-8-amd64.iso /mnt/iso
-cp /mnt/iso/live/filesystem.squashfs ./
 cp /mnt/iso/live/initrd.img $dest/
 cp /mnt/iso/live/vmlinuz $dest/
+sudo unsquashfs -f -d $tmp /mnt/iso/live/filesystem.squashfs
 sudo umount /mnt/iso
-sudo unsquashfs -f -d $tmp filesystem.squashfs
-sudo rm filesystem.squashfs
-sudo mkdir $tmp/root/.ssh
+sudo rm -f $dest/filesystem.squashfs clonezilla*.iso
+sudo mkdir -p $tmp/root/.ssh
 sudo cp ssh/* $tmp/root/.ssh
 sudo chown root:root $tmp/root/.ssh
+sudo chmod 700 $tmp/root/.ssh
+sudo chmod 600 $tmp/root/.ssh/id_rsa
+sudo chmod 644 $tmp/root/.ssh/id_rsa.pub
 sudo mksquashfs $tmp $dest/filesystem.squashfs -b 1024k -comp xz -Xbcj x86 -e boot
 sudo rm -rf $tmp
---
+++ install-grub-amd64-files.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+tmp=$$
+cp etc/apt/sources.list /etc/apt/
+dpkg --add-architecture amd64
+apt update
+cd /root
+apt download grub-efi-amd64-bin:amd64
+mkdir /tmp/$tmp
+dpkg-deb -x grub-efi-amd64-bin*.deb /tmp/$tmp
+cp -rf /tmp/$tmp/usr/lib/grub/x86_64-efi /srv/tftp
+rm -rf /tmp/$tmp
